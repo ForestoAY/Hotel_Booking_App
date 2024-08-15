244318c8d19b9a39048db118013ad829e5bd812d
ForestoAY <forestoay@gmail.com>
2024-08-15 14:02:36 +0700

    filter hotel by rating

--- controllers/HotelController.js
+++ controllers/HotelController.js
@@ -2,8 +2,9 @@ const { Hotel, User, Room } = require('../models');
 
 class HotelController {
   static async readHotel(req, res){
+    const { filter } = req.query;
     try {
-      const hotel = await Hotel.findAll();
+      const hotel = await Hotel.filterByStars(filter);
       const user = await User.findByPk(req.session.userId);
       // res.send(user);
       res.render('Hotel', { hotel, user });
