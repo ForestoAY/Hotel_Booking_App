34eff406e6c1e02342e173d52bbdc4f03886c064
ForestoAY <forestoay@gmail.com>
2024-08-15 15:30:24 +0700

    checkpoint

--- controllers/HotelController.js
+++ controllers/HotelController.js
@@ -1,4 +1,5 @@
-const { Hotel, User, Room } = require('../models');
+const { Op } = require('sequelize');
+const { Hotel, User, Room, Reservation } = require('../models');
 
 class HotelController {
   static async readHotel(req, res){
@@ -6,7 +7,6 @@ class HotelController {
     try {
       const hotel = await Hotel.filterByStars(filter);
       const user = await User.findByPk(req.session.userId);
-      // res.send(user);
       res.render('Hotel', { hotel, user });
     } catch (err) {
       res.send(err);
@@ -29,19 +29,52 @@ class HotelController {
     }
   }
 
-  static async getReserveRoom(req, res){
+  static async bookingRoom(req, res){
+    const { idRoom } = req.params
     try {
-      
+      const room = await Room.findByPk(idRoom);
+      const user = await User.findByPk(req.session.userId);
+      res.render('Booking', { room, user });
     } catch (err) {
       res.send(err);
     }
   }
 
-  static async postReserveRoom(req, res){
+  static async postBookingRoom(req, res){
+    const { UserId, RoomId, checkIn, checkOut, totalPrice, status } = req.body
     try {
-      
+      await Reservation.create({ UserId, RoomId, checkIn, checkOut, totalPrice, status });
+      await Room.update({status: 'Occupied'});
+      res.redirect(`/hotel/reservation/${UserId}`);
     } catch (err) {
       res.send(err);
+    }
+  }
+
+  static async readReservation(req, res){
+    const { id } = req.params
+    try {
+      const user = await User.findByPk(req.session.userId);
+      const data = await Reservation.findAll({
+        order: [['id','asc']],
+        where: {
+          UserId:{
+            [Op.eq]: id
+          }
+        }
+      })
+      res.send(data);
+      // res.render('Reservation', { data, user });
+    } catch (err) {
+      res.send(err.name)
+    }
+  }
+
+  static async getCheckout(req, res){
+    try {
+      await Room.update({status: 'available'});
+    } catch (err) {
+      res.send(err)
     }
   }
 }
